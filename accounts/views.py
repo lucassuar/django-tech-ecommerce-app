a32dbcef32cb7731f759f07577120e37c6b5a805
Lucas <suarez.luks@gmail.com>
2019-04-27 14:04:46 +0000

    Login page, views and urls created

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 
 def index(request):
-    """Return the index.html file"""
+    """Index.html file return"""
     return render(request,  'index.html')
     
 def logout(request):
-    """Log the user out"""
+    """Logout the user"""
     auth.logout(request)
     messages.success(request, "You have successfully been logged out")
     return redirect(reverse('index'))
+    
+def login(request):
+    """Login page return"""
+    return render(request, 'login.html')
