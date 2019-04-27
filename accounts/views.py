7513df18c2578725d9aae7a1216e21ee172ec170
Lucas <suarez.luks@gmail.com>
2019-04-27 15:14:39 +0000

    login required and loggic modified to better redirect login/logout users

--- accounts/views.py
+++ accounts/views.py
@@ -1,30 +1,36 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 def index(request):
     """Index.html file return"""
     return render(request,  'index.html')
-    
+  
+  
+@login_required   
 def logout(request):
     """Logout the user"""
     auth.logout(request)
     messages.success(request, "You have successfully been logged out")
     return redirect(reverse('index'))
-    
+
+
 def login(request):
     """Return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
                                     password=request.POST['password'])
-            
+            messages.success(request, "You have successfully logged in!")
 
             if user:
                 auth.login(user=user, request=request)
-                messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
     else:
