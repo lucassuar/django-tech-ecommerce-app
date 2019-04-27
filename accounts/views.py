52243958e21655c924ce09cce5476283eb5b1224
Lucas <suarez.luks@gmail.com>
2019-04-27 13:39:47 +0000

    Logout views, and url routes created

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth
 
 def index(request):
     """Return the index.html file"""
     return render(request,  'index.html')
+    
+def logout(request):
+    """Log the user out"""
+    auth.logout(request)
+    return redirect(reverse('index'))
