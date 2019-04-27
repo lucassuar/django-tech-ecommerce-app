29faccf087070bc0ff75c2b080320ad0f544d190
Lucas <suarez.luks@gmail.com>
2019-04-27 14:26:55 +0000

    Login logic created on views and form added to hetml login file

--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,20 @@ def logout(request):
     return redirect(reverse('index'))
     
 def login(request):
-    """Login page return"""
-    login_form = UserLoginForm()
+    """Return a login page"""
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password'])
+            
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
