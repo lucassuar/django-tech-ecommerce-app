d56e8bb38d939c6a432437ad2d24cf9caa38900e
Lucas <suarez.luks@gmail.com>
2019-04-28 16:22:50 +0000

    cart funcionalities created and linked togther. Created context.py and created the logic for views and set urls files

---
+++ cart/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect, reverse
+
+# Create your views here.
+def view_cart(request):
+    """A View that shows the cart details page"""
+    return render(request, "cart.html")
+
+
+def add_to_cart(request, id):
+    """Add a quantity of the specified product to the cart"""
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
+
+
+def adjust_cart(request, id):
+    """
+    Change quantity of a product to the specified amount
+    """
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_forms_bootstrap',
     'accounts',
     'products',
+    'cart',
     
 ]
 
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
             ],
         },
     },
