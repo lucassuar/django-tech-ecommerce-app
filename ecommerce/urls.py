c88d5278d5f7d2c1cd71501edc862bfef901daad
Lucas <suarez.luks@gmail.com>
2019-04-29 09:58:29 +0000

    Checkout urls added in both folders

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,6 +5,7 @@ from products import urls as urls_products
 from cart import urls as urls_cart
 from search import urls as urls_search
 from products.views import all_products
+from checkout import urls as urls_checkout
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -15,5 +16,6 @@ urlpatterns = [
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
     url(r'^search/', include(urls_search)),
+    url(r'^checkout/', include(urls_checkout)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
