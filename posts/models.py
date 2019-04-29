324976d316a6ef8faee244ccd42abac55fd22a80
Lucas <suarez.luks@gmail.com>
2019-04-29 13:20:17 +0000

    Post model created with blog post structure and database calls

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    image = models.ImageField(upload_to="images", blank=True, null=True)
+
+    def __unicode__(self):
+        return self.title
