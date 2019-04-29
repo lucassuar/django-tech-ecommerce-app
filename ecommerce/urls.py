147d8df2bc106de467e0332eecdaa6dcc6d1749a
Lucas <suarez.luks@gmail.com>
2019-04-29 14:08:37 +0000

    Blog posts urls created on post folder and main ecommerce folder

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,6 +7,8 @@ from search import urls as urls_search
 from products.views import all_products
 from checkout import urls as urls_checkout
 from django.views import static
+from django.views.generic import RedirectView
+from django.views.static import serve
 from .settings import MEDIA_ROOT
 
 urlpatterns = [
@@ -17,5 +19,7 @@ urlpatterns = [
     url(r'^cart/', include(urls_cart)),
     url(r'^search/', include(urls_search)),
     url(r'^checkout/', include(urls_checkout)),
+    url(r'^$', RedirectView.as_view(url='posts/')),
+    url(r'^posts/', include('posts.urls')),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
