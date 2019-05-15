df38f9df1e1db6533f1b8e06cbda69e9a2dc6885
Lucas <suarez.luks@gmail.com>
2019-05-15 07:26:53 +0000

    Sidebar fixed on blogpost list page and individual blog post page. Also added some css styles for it

--- suggestions/models.py
+++ suggestions/models.py
@@ -8,6 +8,7 @@ class Suggestion(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     image = models.ImageField(upload_to="img", blank=True, null=True)
+    upvotes = models.IntegerField(default=0)
 
     def __unicode__(self):
         return self.title
--- suggestions/urls.py
+++ suggestions/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', get_suggestions, name='get_suggestions'),
     url(r'^(?P<pk>\d+)/$', suggestion_detail, name='suggestion_detail'),
     url(r'^new/$', create_or_edit_suggestion, name='new_suggestion'),
-    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_suggestion, name='edit_suggestion')
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_suggestion, name='edit_suggestion'),
+    #url(r'^$', upvotes, name='upvotes'),
 ]
